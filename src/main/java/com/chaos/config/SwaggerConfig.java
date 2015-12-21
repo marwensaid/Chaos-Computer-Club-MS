e089671d9438137fae46a72bce89342a13bff1db
marwensaid <marwensaidi1@gmail.com>
2015-12-21 19:06:37 +0100

    feat(CCC-blog) add config {swagger ...}  fix swagger V2--MS

---
+++ src/main/java/com/chaos/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.chaos.config;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * Created by marwen on 21/12/15.
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(paths())
+                .build()
+                .apiInfo(apiInfo())
+
+                ;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("CCC-blog", "Chaos Computer Club", "1.0.0", null, "marwensaidi1@gmail.com", null, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Predicate<String> paths() {
+        return or(
+                regex("/*.*"));
+    }
+}
---
+++ src/main/java/com/chaos/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.chaos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by marwen on 21/12/15.
+ */
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
+        //registry.addViewController("/admin").setViewName("admin/index.html");
+    }
+}
