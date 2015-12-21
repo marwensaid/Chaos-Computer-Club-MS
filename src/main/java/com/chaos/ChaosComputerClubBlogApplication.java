7a187ef8abdda880b3605f1616e619c7a9768533
marwensaid <marwensaidi1@gmail.com>
2015-12-21 19:07:54 +0100

    feat(CCC-blog)  defined auto-config--MS

--- src/main/java/com/chaos/ChaosComputerClubBlogApplication.java
+++ src/main/java/com/chaos/ChaosComputerClubBlogApplication.java
@@ -2,8 +2,11 @@ package com.chaos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+
 public class ChaosComputerClubBlogApplication {
 
     public static void main(String[] args) {
