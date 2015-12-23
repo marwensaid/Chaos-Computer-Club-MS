dc99fa940655ec683b0106240a558edbc2d22fa4
marwensaid <marwensaidi1@gmail.com>
2015-12-23 19:43:09 +0100

    refactor(CCC-blog)  refactor interface // last commit is a feat not a refactor--MS

--- src/main/java/com/chaos/interfacesRepository/EmailSubscriberRepository.java
+++ src/main/java/com/chaos/interfacesRepository/EmailSubscriberRepository.java
@@ -1,18 +1,15 @@
 package com.chaos.interfacesRepository;
 
 import com.chaos.entities.EmailSubscriber;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 /**
  * Created by marwen on 21/12/15.
  */
-public interface EmailSubscriberRepository {
-    EmailSubscriber save(EmailSubscriber subscriber);
-
-    List<EmailSubscriber> findAll();
+public interface EmailSubscriberRepository extends JpaRepository<EmailSubscriber, Integer> {
 
     EmailSubscriber findByEmail(String email);
 
-    void delete(EmailSubscriber subscriber);
 }
--- src/main/java/com/chaos/interfacesRepository/TagRepository.java
+++ src/main/java/com/chaos/interfacesRepository/TagRepository.java
@@ -3,8 +3,6 @@ package com.chaos.interfacesRepository;
 import com.chaos.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 /**
  * Created by marwen on 23/12/15.
  */
