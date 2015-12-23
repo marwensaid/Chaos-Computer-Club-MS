fd74eb5d7ff0bede8a59bc3ccdf65a2465c112a7
marwensaid <marwensaidi1@gmail.com>
2015-12-23 19:52:43 +0100

    feat(CCC-blog)  add SecurityUser--MS

--- src/main/java/com/chaos/security/SecurityUser.java
+++ src/main/java/com/chaos/security/SecurityUser.java
@@ -1,16 +1,61 @@
 package com.chaos.security;
 
 import com.chaos.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 /**
  * Created by marwen on 21/12/15.
  */
-public class SecurityUser {
-    private String username;
+public class SecurityUser implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
     private User domainUser;
 
+    public SecurityUser(User domainUser) {
+        this.domainUser = domainUser;
+    }
+
+    public void setDomainUser(User domainUser) {
+        this.domainUser = domainUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return domainUser.getPassword();
+    }
+
+    @Override
     public String getUsername() {
-        return username;
+        return domainUser.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public User getDomainUser() {
