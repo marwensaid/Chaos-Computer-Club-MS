fe487bca67de9cd34ceb8bafc19b6e3cac9107cc
marwensaid <marwensaidi1@gmail.com>
2015-12-25 07:35:21 +0100

    feat(CCC-blog)  impl http entry point (auth) --MS

--- src/main/java/com/chaos/security/HttpAuthenticationEntryPoint.java
+++ src/main/java/com/chaos/security/HttpAuthenticationEntryPoint.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class HttpAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
 
     }
 }
