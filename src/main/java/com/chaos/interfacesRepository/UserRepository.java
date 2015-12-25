91a7ff43745cc72bf5b1d61db86b3e7d674c41e6
marwensaid <marwensaidi1@gmail.com>
2015-12-25 07:40:21 +0100

    feat(CCC-blog)  impl security class customUserDS and and method to interface for impl --MS

--- src/main/java/com/chaos/interfacesRepository/UserRepository.java
+++ src/main/java/com/chaos/interfacesRepository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmailAndPassword(String email, String password);
+    User findByEmail(String email);
+
 }
--- src/main/java/com/chaos/security/CustomUserDetailsService.java
+++ src/main/java/com/chaos/security/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.chaos.security;
 
+import com.chaos.entities.User;
+import com.chaos.interfacesRepository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +11,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  * Created by marwen on 25/12/15.
  */
 public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        System.out.println("Email: " + email + ", User: " + user);
+        if (user == null) throw new UsernameNotFoundException("User not found");
+        return new SecurityUser(user);
     }
 }
