30c0e3af1f01e41052e536d542a8a0aa3254707b
marwensaid <marwensaidi1@gmail.com>
2015-12-25 07:46:19 +0100

    feat(CCC-blog)  add authentification success and failure --MS

--- src/main/java/com/chaos/security/AuthenticationFailureHandlerCCC.java
+++ src/main/java/com/chaos/security/AuthenticationFailureHandlerCCC.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by marwen on 25/12/15.
@@ -14,6 +15,10 @@ import java.io.IOException;
 public class AuthenticationFailureHandlerCCC implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
+        PrintWriter writer = httpServletResponse.getWriter();
+        writer.write(e.getMessage());
+        writer.flush();
     }
 }
--- src/main/java/com/chaos/security/AuthenticationSuccessHandlerCCC.java
+++ src/main/java/com/chaos/security/AuthenticationSuccessHandlerCCC.java
@@ -1,5 +1,10 @@
 package com.chaos.security;
 
+import com.chaos.entities.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -7,13 +12,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by marwen on 25/12/15.
  */
 public class AuthenticationSuccessHandlerCCC implements AuthenticationSuccessHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationSuccessHandlerCCC.class);
+    private ObjectMapper mapper;
+
+    @Autowired
+    public AuthenticationSuccessHandlerCCC(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
 
+        SecurityUser userDetails = (SecurityUser) authentication.getPrincipal();
+        User user = userDetails.getDomainUser();
+
+        LOGGER.info(userDetails.getUsername() + " got is connected ");
+
+        PrintWriter writer = httpServletResponse.getWriter();
+        mapper.writeValue(writer, user);
+        writer.flush();
     }
 }
