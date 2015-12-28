f18bc7164ce259fb325094bf478b96c8646ca7ad
marwensaid <marwensaidi1@gmail.com>
2015-12-28 20:31:13 +0100

    refactor(CCC-blog)  FO login controller and view  --MS

--- src/main/webApp/scripts/controller/LoginController.js
+++ src/main/webApp/scripts/controller/LoginController.js
@@ -1,3 +1,34 @@
 /**
  * Created by marwen on 28/12/15.
  */
+
+var cccBlog = angular.module('cccBlog');
+
+cccBlog.controller('LoginController',
+    ['$rootScope', '$scope', '$http', '$state', '$location', '$cookies', 'UserService', 'UtilService',
+        function ($rootScope, $scope, $http, $state, $location, $cookies, UserService, UtilService) {
+            $scope.loginUser = {};
+
+            $scope.authenticateUser = function () {
+
+                $scope.loginFailed = false;
+                $http({
+                    method: 'POST',
+                    url: 'authenticate',
+                    data: $.param($scope.loginUser),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
+
+                    .then(function (response) {
+                        //UtilService.notifyInfo('Login successful');
+                        UserService.setUser(response.data);
+                        $state.transitionTo('home');
+                    }, function (response) {
+                        console.log('Login failed');
+                        $scope.loginFailed = true;
+                        UtilService.notifyError('Invalid Login Credentials');
+                    });
+            };
+
+        }
+    ]);
