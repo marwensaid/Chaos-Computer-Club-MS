936521a06a6d4fe7f81d9df79509d1161f9d5d66
marwensaid <marwensaidi1@gmail.com>
2015-12-28 20:41:27 +0100

    feat(CCC-blog)  terminated with newPost controller and view , finished FO--MS

--- src/main/webApp/scripts/controller/NewPostController.js
+++ src/main/webApp/scripts/controller/NewPostController.js
@@ -4,3 +4,22 @@
 
 var cccBlog = angular.module('cccBlog');
 
+cccBlog.controller('NewPostController', [ '$scope','$http', '$stateParams', '$location', 'UtilService',
+    function ($scope, $http, $stateParams, $location, UtilService) {
+
+        $scope.newPost = {};
+
+        $scope.createPost = function(){
+            $scope.newPost.content = $('#contentEditor').code();
+            $http.post('api/admin/posts/', $scope.newPost)
+                .success(function(data, status, headers, config){
+                    UtilService.notifyInfo('Post saved successfully');
+                    $scope.newPost = {};
+                    $('#contentEditor').code('');
+                })
+                .error(function(data, status, headers, config){
+                    UtilService.notifyError('Problem in saving post');
+                });
+        }
+
+    }]);
