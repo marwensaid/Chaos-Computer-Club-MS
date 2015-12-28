4b03d670358b1fd9fed64269f7dacdb196f4c235
marwensaid <marwensaidi1@gmail.com>
2015-12-28 20:34:48 +0100

    refactor(CCC-blog)  FO home controller and view // refactor login.html --MS

--- src/main/webApp/scripts/controller/HomeController.js
+++ src/main/webApp/scripts/controller/HomeController.js
@@ -1,3 +1,37 @@
 /**
  * Created by marwen on 28/12/15.
  */
+
+var cccBlog = angular.module('cccBlog');
+
+cccBlog.controller('HomeController',
+    ['$scope', '$rootScope', '$http', '$location', '$sce', 'PostsService', 'UtilService',
+        function ($scope, $rootScope, $http, $location, $sce, PostsService, UtilService) {
+
+            $scope.loadPosts = function (page) {
+                var pageSize = 5;
+
+                PostsService.loadPosts(page, pageSize)
+                    .then(
+                        function (data, status, headers, config) {
+                            var posts = data.posts;
+                            angular.forEach(posts, function (post) {
+                                post.contentPreview = $sce.trustAsHtml(post.contentPreview);
+                            });
+                            $scope.posts = posts;
+                            $scope.postsPagination = {
+                                hasNextPage: data.hasNextPage,
+                                hasPrevPage: data.hasPrevPage,
+                                currentPage: data.currentPage
+                            };
+                        },
+                        function (data, status, headers, config) {
+                            UtilService.notifyError('Problem in loading posts');
+                        }
+                    );
+            };
+
+            $scope.loadPosts(0);
+
+        }]);
+
