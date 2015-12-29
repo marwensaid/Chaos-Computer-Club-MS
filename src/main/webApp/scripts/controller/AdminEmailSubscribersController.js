6646f2c7acb5b23a412f4efbfe57bdeac8d3bfea
marwensaid <marwensaidi1@gmail.com>
2015-12-29 19:43:18 +0100

    feat(CCC-blog)  add email subs , this func used like username--MS

--- src/main/webApp/scripts/controller/AdminEmailSubscribersController.js
+++ src/main/webApp/scripts/controller/AdminEmailSubscribersController.js
@@ -1,3 +1,44 @@
 /**
  * Created by marwen on 28/12/15.
  */
+
+var cccBlog = angular.module('cccBlog');
+
+cccBlog.controller('AdminEmailSubscribersController',
+    [ '$scope', '$rootScope', '$http', '$location','$sce', 'PostsService','UtilService',
+        function ($scope, $rootScope, $http, $location, $sce,PostsService, UtilService)
+        {
+            console.log('AdminEmailSubscribersController');
+
+            $scope.subscribers = [];
+
+            $scope.loadSubscribers = function()
+            {
+                $http.get('api/admin/emailSubscribers')
+                    .success(function(data, status, headers, config){
+                        $scope.subscribers = data;
+                    })
+                    .error(function(data, status, headers, config){
+                        UtilService.notifyError('Problem in loading subscribers');
+                    });
+            }
+            $scope.newSubscriber = {};
+
+            $scope.addSubscriber = function()
+            {
+                $http.post('api/emailSubscribers', $scope.newSubscriber)
+                    .success(function(data, status, headers, config){
+                        UtilService.notifyInfo('Subscriber added successfully');
+                        $scope.loadSubscribers();
+                        $scope.newSubscriber = {};
+                    })
+                    .error(function(data, status, headers, config){
+                        UtilService.notifyError('Problem in adding subscriber');
+                    });
+            }
+
+            $scope.loadSubscribers();
+
+        }
+    ]
+);
