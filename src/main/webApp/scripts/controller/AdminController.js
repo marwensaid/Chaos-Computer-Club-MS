74e165d580097f83896c35a5ec5d05a9a67c888b
marwensaid <marwensaidi1@gmail.com>
2015-12-29 19:35:38 +0100

    feat(CCC-blog)  first impl the adminController and V--MS

--- src/main/webApp/scripts/controller/AdminController.js
+++ src/main/webApp/scripts/controller/AdminController.js
@@ -1,3 +1,17 @@
 /**
+ *
  * Created by marwen on 28/12/15.
  */
+
+var cccBlog = angular.module('cccBlog');
+
+cccBlog.controller('AdminController',
+    [ '$scope', '$rootScope', '$http', '$location','$sce', 'PostsService','UtilService',
+        function ($scope, $rootScope, $http, $location, $sce,PostsService, UtilService)
+        {
+            console.log('admin controller');
+
+
+        }
+    ]
+);
